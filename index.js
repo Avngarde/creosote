64e40d6a8dce1658da884f65049540716a7bdd1f
ShamanDev <pivotguy888@gmail.com>
2021-07-05 12:35:49 +0200

    Init app

---
+++ index.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+    width: 800,
+  });
+
+  mainWindow.loadFile("index.html");
+
+  mainWindow.webContents.openDevTools();
+}
+
+
+app.on("ready", () => {
+  createWindow();
+
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
