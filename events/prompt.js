ab6b0bc3e7d2708ee70645b740b08e72634971a5
ShamanDev <pivotguy888@gmail.com>
2021-07-19 22:56:23 +0200

    Added prompt

---
+++ events/prompt.js
@@ -0,0 +1,7 @@
+function showLoading() {
+    document.getElementById("prompt").style = "visibility: visible;";
+}
+
+function hideLoading() {
+    document.getElementById("prompt").style = "visibility: hidden;";
+}
